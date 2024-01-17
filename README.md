0472d7269a04518d5bc7df4dd793d2df577e7aa6
Markos Herm <markos.herm@tptlive.ee>
2024-01-17 14:59:04 +0200

    added html

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # vm22markoshermlearnexpress
+
+## install
+
+`npm i`
+
+## run
+
+`node server.js`
--- server.js
+++ server.js
@@ -1,1 +1,15 @@
 console.log('hello terminal');
+
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+  console.log('somebody cum here')
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port http://localhost:${port}`);
+});
+
