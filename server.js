4952239b4779897670fbd71bdce32043d515689b
Markos Herm <markos.herm@tptlive.ee>
2024-01-17 15:36:09 +0200

    YAAAS nodemon

--- server.js
+++ server.js
@@ -5,8 +5,11 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html');
-  console.log('somebody cum here')
+    res.sendFile(__dirname + '/index.html');
+  });
+
+app.get('/page2', (req, res) => {
+  res.sendFile(__dirname + '/page2.html');
 });
 
 app.listen(port, () => {
